3ac1a7a56f1bc1a6ad391fbd0056ced4878118bc
crazykid080 <mobile@crazykid080.com>
2020-05-26 19:26:25 -0700

    Work on datbase framework

--- database.py
+++ database.py
@@ -1,11 +1,16 @@
 from secret import secret_key
-import sqlalchemy
+import sqlalchemy as db
 from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
 
-
-if __name__ == '__main__':
+def __init__():
+	engine = db.create_engine('sqlite:///main.sqlite')
+	connection = engine.connect()
 	metadata = MetaData()
 	users = Table('users', metadata, 
 	Column('id', Integer, primary_key=True),
-	Column('username', String),
-	Column('password', String) )
+	Column('username', String(25)),
+	Column('password', String(70)) )
+
+if __name__ == '__main__':
+	__init__()
+	insertion = None
