aa58fcbe06bb1bc43839898ba1b0a9fe68cebbd4
crazykid080 <10173541+crazykid080@users.noreply.github.com>
2020-06-03 18:44:46 -0700

    Added ability to choose database in management.py Closes #9

--- management.py
+++ management.py
@@ -40,11 +40,11 @@ def add_transfer(testdb, origin, reciever, amount):
 	testdb.transfer(origin, reciever, amount)
 
 if __name__ == "__main__":
-	if(len(sys.argv) < 2):
+	if(len(sys.argv) < 3):
 		print("Not enough arguments")
 		exit()
 	command = sys.argv[1]
-	testdb = database.database('test')
+	testdb = database.database(sys.argv[2])
 	if(command == 'get_users'):
 		get_users(testdb)
 		exit()
@@ -52,33 +52,33 @@ if __name__ == "__main__":
 		get_tables(testdb)
 		exit()
 	if(command == 'get_balance'):
-		if(len(sys.argv) < 3):
-			print("Not enough arguments")
-			exit()
-		get_balance(testdb, sys.argv[2])
-	if(command == 'transfer'):
-		if(len(sys.argv) < 5):
-			print("Not enough arguments")
-			exit()
-		add_transfer(testdb, sys.argv[2], sys.argv[3], int(sys.argv[4]))
-	if(command == 'add_transaction'):
 		if(len(sys.argv) < 4):
 			print("Not enough arguments")
 			exit()
-		result = add_transaction(testdb, sys.argv[2], sys.argv[3])
+		get_balance(testdb, sys.argv[3])
+	if(command == 'transfer'):
+		if(len(sys.argv) < 6):
+			print("Not enough arguments")
+			exit()
+		add_transfer(testdb, sys.argv[3], sys.argv[4], int(sys.argv[5]))
+	if(command == 'add_transaction'):
+		if(len(sys.argv) < 5):
+			print("Not enough arguments")
+			exit()
+		result = add_transaction(testdb, sys.argv[3], sys.argv[4])
 		print(result)
 		exit()
 	if(command == 'login'):
-		if(len(sys.argv) < 4):
-			print("Not enough arguments")
-			exit()
-		result = login(testdb, sys.argv[2], sys.argv[3])
-		print(result)
-		exit()
-	if(command == 'add_user'):
 		if(len(sys.argv) < 5):
 			print("Not enough arguments")
 			exit()
-		result = add_user_test(testdb, sys.argv[2], sys.argv[3], sys.argv[4])
+		result = login(testdb, sys.argv[3], sys.argv[4])
+		print(result)
+		exit()
+	if(command == 'add_user'):
+		if(len(sys.argv) < 6):
+			print("Not enough arguments")
+			exit()
+		result = add_user_test(testdb, sys.argv[3], sys.argv[4], sys.argv[5])
 		print(result)
 		exit()
