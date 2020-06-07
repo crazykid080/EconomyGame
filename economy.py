7c55ba6513958d98cb1a01b5be20d1ce8fe4790f
crazykid080 <mobile@crazykid080.com>
2020-06-06 22:35:12 -0700

    Fixed convert function again, started work on json

--- economy.py
+++ economy.py
@@ -1,6 +1,12 @@
-def convert_currency(self, amount):
+import json
+
+def convert_currency(amount):
 	units = amount * 10000
 	return units
+	
+def convert_units(units):
+	currency = units / 10000
+	return currency
 
 class economy:
 	work_payment = 0.007
@@ -15,3 +21,10 @@ class economy:
 		units = round(units, 4) #Prevent floating point errors
 		budget += round(taxed, 4)
 		return units
+		
+	def read_state(self, file):
+		json_data = json.read(file+".json")
+		pass
+	
+	def write_state(self, file):
+		pass
